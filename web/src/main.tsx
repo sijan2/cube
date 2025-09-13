b2b9ec4f461d777afa10191585f7b1a5958975ac
Sijan Mainali <sijanmainali2@gmail.com>
2025-09-13 14:08:40 -0400

    tailwindcssXshadcn setup

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -5,7 +5,7 @@ import { RouterProvider, createRouter } from '@tanstack/react-router'
 // Import the generated route tree
 import { routeTree } from './routeTree.gen'
 
-import './styles.css'
+import '../globals.css'
 import reportWebVitals from './reportWebVitals.ts'
 
 // Create a new router instance
