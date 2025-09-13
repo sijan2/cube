ae9cba466078ab296fea91d61a5681c5c7ba6597
Sijan Mainali <sijanmainali2@gmail.com>
2025-09-13 14:10:56 -0400

    added calendar layout from origin ui

---
+++ web/src/next.config.mjs
@@ -0,0 +1,14 @@
+const nextConfig = {
+  assetPrefix: "/exp6-static",
+  transpilePackages: ["@workspace/ui"],
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'raw.githubusercontent.com',
+      },
+    ],    
+  },  
+}
+
+export default nextConfig
